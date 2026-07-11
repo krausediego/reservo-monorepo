c6e9135bae14442f1f7046a281475bcae6a1ec9f
Diego Emanuel Krause <diegoemanuelk@gmail.com>
2026-07-11 15:12:55 -0300

    feat: add update professional endpoint

--- packages/schemas/src/professional/index.ts
+++ packages/schemas/src/professional/index.ts
@@ -2,3 +2,4 @@ export * from "./create-professional.schemas";
 export * from "./list-professionals.schemas";
 export * from "./professional.schemas";
 export * from "./get-professional.schemas";
+export * from "./update-professional.schemas";
--- packages/types/src/professional/index.ts
+++ packages/types/src/professional/index.ts
@@ -1,3 +1,5 @@
 export * from "./create-professional.types";
 export * from "./list-professionals.types";
 export * from "./get-professional.types";
+export * from "./update-professional.types";
+export * from "./professional.types";
